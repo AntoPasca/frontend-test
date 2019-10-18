4a610deb2c6b3a5ad9cdd59e31a1dcaa9b6d4b17
Antonio Pasca <a.pasca@almaviva.it>
2019-10-18 16:39:40 +0200

    [ENVIRONMENT] - Modifica path server be + gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://websocket-chat-websocketchat.apps.us-west-2.online-starter.openshift.com/'
+  backendUrl: 'https://chat-websocket-be.herokuapp.com/'
 };
